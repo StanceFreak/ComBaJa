6ced32fdeef5a1bd1f561c42fc5933933b7b2f2d
StanceFreak <jombor.army67@gmail.com>
2024-10-08 16:14:33 +0700

    feat(dependency & permissions): added necessary dependencies and permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
+    val localPropertiesFile = rootProject.file("local.properties")
+    val localProperties = Properties()
+    localProperties.load(localPropertiesFile.inputStream())
     namespace = "com.stancefreak.combaja"
     compileSdk = 34
 
@@ -21,8 +26,16 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", localProperties["BASE_URL"].toString())
+            buildConfigField("String", "API_KEY", localProperties["API_KEY"].toString())
+            buildConfigField("String", "ACCESS_TOKEN", localProperties["ACCESS_TOKEN"].toString())
+        }
         release {
             isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", localProperties["BASE_URL"].toString())
+            buildConfigField("String", "API_KEY", localProperties["API_KEY"].toString())
+            buildConfigField("String", "ACCESS_TOKEN", localProperties["ACCESS_TOKEN"].toString())
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -38,6 +51,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -59,6 +73,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +81,27 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //splash API
+    implementation(libs.androidx.core.splashscreen)
+
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //coroutine & livedata
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    //load image from URL
+    implementation(libs.coil.compose)
+    implementation(libs.logging.interceptor)
+
+    //viewpager
+    implementation(libs.androidx.foundation)
 }
